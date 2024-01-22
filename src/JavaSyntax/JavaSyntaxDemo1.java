1d64e81e00655add2fe056119e381373ae445c17
Bhushan-714 <bhushanpatil.rcpit@gmail.com>
2024-01-22 16:20:46 +0530

    Commit

---
+++ src/JavaSyntax/JavaSyntaxDemo1.java
@@ -0,0 +1,12 @@
+package JavaSyntax;
+
+public class JavaSyntaxDemo1 {
+    public static void main(String[] args) {
+        System.out.println("Hello my name is Sagar");
+
+        System.out.print("Hello me name is Sagar");
+
+        int a = 5;
+        System.out.printf("\nI am %d years old",a);
+    }
+}
